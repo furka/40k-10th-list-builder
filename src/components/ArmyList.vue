5ac6590f27ab8e07bb98af338cc9cae67736ec95
Roger <roger.lampe@gmail.com>
2023-07-28 19:51:33 -0400

    sort army list

--- src/components/ArmyList.vue
+++ src/components/ArmyList.vue
@@ -19,39 +19,74 @@ const points = computed(() => {
   );
 });
 
+
+function sortName(a, b) {
+  const aa = a.name.toLowerCase();
+  const bb = b.name.toLowerCase();
+  return aa < bb ? -1 : aa > bb ? 1 : 0;
+}
+
+function sortPointsMin(a, b) {
+  return a.points < b.points ? -1 : a.points > b.points ? 1 : 0;
+}
+
+function sortPointsMax(a, b) {
+  return a.points < b.points ? 1 : a.points > b.points ? -1 : 0;
+}
+
 const emptySpace = computed(() => {
   return (
     Math.max(0, props.appData.currentList.maxPoints - points.value) *
-      scale.value +
+    scale.value +
     "px"
   );
 });
 </script>
 
 <template>
-  <draggable
-    v-model="props.appData.currentList.units"
-    group="units"
-    animation="150"
-    item-key="id"
-    class="army-list"
-  >
-    <template #item="{ element, index }">
-      <ArmyListUnit :unit="element" :scale="scale" :app-data="props.appData" />
-    </template>
-  </draggable>
+  <div class="sidebar">
+    <div class="sidebar-sort-label">Sort By:</div>
+    <span class="sidebar-sort-box">
+      <button @click="props.appData.currentList.units.sort(sortPointsMax)">Points Max</button>
+      <button @click="props.appData.currentList.units.sort(sortPointsMin)">Points Min</button>
+      <button @click="props.appData.currentList.units.sort(sortName)">Name</button>
+    </span>
+    <draggable v-model="props.appData.currentList.units" group="units" animation="150" item-key="id"
+      class="sidebar-army-list">
+      <template #item="{ element }">
+        <ArmyListUnit :unit="element" :scale="scale" :app-data="props.appData" />
+      </template>
+    </draggable>
+  </div>
 </template>
 
 <style scoped lang="scss">
-.army-list {
-  background-image: url(../assets/bg-dark.png);
-  background-size: 100% 100%;
-  display: flex;
-  flex-direction: column;
-  flex-shrink: 0;
-  justify-content: flex-end;
-  max-width: calc(100vw - 300px);
-  padding-top: v-bind("emptySpace");
+.sidebar {
   width: 250px;
+
+  &-army-list {
+    display: flex;
+    flex-direction: column;
+    flex-shrink: 0;
+    justify-content: flex-end;
+    background-image: url(../assets/bg-dark.png);
+    max-width: calc(100vw - 300px);
+    background-size: 100% 100%;
+    padding-top: v-bind("emptySpace");
+  }
+
+  &-sort-box {
+    display: flex;
+    justify-content: space-around;
+    background-color: #333;
+    color: white;
+  }
+
+  &-sort-label {
+    display: flex;
+    justify-content: space-evenly;
+    background-color: #333;
+    color: white;
+  }
 }
 </style>
