8b2c45c77335fa609e3a4e07f80dcd438233f515
furka <thomas-vranckx@gmail.com>
2025-10-22 13:24:04 +0100

    MFM 3.5

--- src/utils/mfm.js
+++ src/utils/mfm.js
@@ -5,10 +5,11 @@ import MFM29 from "../data/munitorum-field-manual/MFM2.9.txt?raw";
 import MFM32 from "../data/munitorum-field-manual/MFM3.2.txt?raw";
 import MFM33 from "../data/munitorum-field-manual/MFM3.3.txt?raw";
 import MFM34 from "../data/munitorum-field-manual/MFM3.4.txt?raw";
+import MFM35 from "../data/munitorum-field-manual/MFM3.5.txt?raw";
 
 const MFM = {};
 
-const imports = [MFM29, MFM32, MFM33, MFM34];
+const imports = [MFM29, MFM32, MFM33, MFM34, MFM35];
 
 imports.forEach((mod) => {
   const { FACTIONS, DATA_SHEETS, MFM_VERSION } = parse(mod);
