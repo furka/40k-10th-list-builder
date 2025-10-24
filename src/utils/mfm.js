90fcd0d0b1bce99d0478ab843407fa34804cbcc3
furka <thomas-vranckx@gmail.com>
2025-10-24 22:12:31 +0100

    consolidate MFM utilities into mfm.js and add getPreviousMFM function

--- src/utils/mfm.js
+++ src/utils/mfm.js
@@ -15,29 +15,99 @@ imports.forEach((mod) => {
   const { FACTIONS, DATA_SHEETS, MFM_VERSION } = parse(mod);
   MFM[MFM_VERSION] = { FACTIONS, DATA_SHEETS, MFM_VERSION };
 
-  if (!MFM.LATEST) {
-    MFM.LATEST = MFM[MFM_VERSION];
-  } else if (MFM_VERSION > MFM.LATEST.MFM_VERSION) {
-    MFM.LATEST = MFM[MFM_VERSION];
+  if (!MFM.CURRENT) {
+    MFM.CURRENT = MFM[MFM_VERSION];
+  } else if (MFM_VERSION > MFM.CURRENT.MFM_VERSION) {
+    MFM.CURRENT = MFM[MFM_VERSION];
   }
 
   if (!MFM.PREVIOUS) {
     MFM.PREVIOUS = MFM[MFM_VERSION];
   } else if (
     MFM_VERSION > MFM.PREVIOUS.MFM_VERSION &&
-    MFM_VERSION < MFM.LATEST.MFM_VERSION
+    MFM_VERSION < MFM.CURRENT.MFM_VERSION
   ) {
     MFM.PREVIOUS = MFM[MFM_VERSION];
   }
 });
 
-Object.defineProperty(MFM, "CURRENT", {
-  get() {
-    return MFM.LATEST; // TODO: allow user to select different mfm
-  },
-  enumerable: true,
-});
-
 deepFreeze(MFM);
 
-export { MFM };
+function getPreviousMFM(currentMFM) {
+  if (!currentMFM) {
+    return null;
+  }
+
+  const versions = Object.keys(MFM)
+    .filter(key => key.startsWith('VERSION'))
+    .sort();
+
+  const currentVersionKey = currentMFM.MFM_VERSION;
+  const currentIndex = versions.indexOf(currentVersionKey);
+
+  return currentIndex > 0 ? MFM[versions[currentIndex - 1]] : null;
+}
+
+function getPoints(unit, mfm = MFM.CURRENT) {
+  const data_sheet = mfm.DATA_SHEETS.find((d) => d.name === unit.name);
+  let option;
+
+  if (!data_sheet) {
+    return -1;
+  }
+
+  if (unit.optionName) {
+    option = data_sheet.sizes.find((s) => s.name === unit.optionName.trim());
+  } else if (unit.models) {
+    option = data_sheet.sizes.find((s) => s.models === unit.models);
+  }
+
+  if (option) {
+    return option.points;
+  }
+
+  return -1;
+}
+
+function getUnitPointsDifference(
+  unit,
+  currentMFM = MFM.CURRENT,
+  previousMFM = MFM.PREVIOUS
+) {
+  const currentUnitPoints = getPoints(unit, currentMFM);
+  const previousUnitPoints = getPoints(unit, previousMFM);
+
+  if (currentUnitPoints === -1 || previousUnitPoints === -1) {
+    return 0;
+  }
+
+  return currentUnitPoints - previousUnitPoints;
+}
+
+function autoUpgradeMFMVersion(list) {
+  if (!changes(list).length) {
+    list.mfm_version = MFM.CURRENT.MFM_VERSION;
+  }
+}
+
+function changes(list) {
+  return list.units
+    .map((u) => {
+      const listMFM = MFM[list.mfm_version] || MFM.CURRENT;
+      const oldPoints = getPoints(u, listMFM);
+      const newPoints = getPoints(u, MFM.CURRENT);
+      const pointsDiff = getUnitPointsDifference(u, MFM.CURRENT, listMFM);
+
+      return {
+        name: u.name,
+        old: oldPoints,
+        new: newPoints,
+        difference: pointsDiff,
+        models: u.models,
+        optionName: u.optionName,
+      };
+    })
+    .filter((i) => i.new !== i.old);
+}
+
+export { MFM, getPreviousMFM, getPoints, getUnitPointsDifference, autoUpgradeMFMVersion, changes };
